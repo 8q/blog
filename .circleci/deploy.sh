f8ec8c5d40f21d3f041b9bd0bdb73290dd33997c
shira <shira7867@gmail.com>
2019-11-28 21:13:28 +0900

    clean deploy dir before copy new files

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -9,6 +9,7 @@ git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
 git config --global user.name $CIRCLE_USERNAME
 git clone -q --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
 
+rm -rf $DEPLOY_DIR/*
 cd $DEPLOY_DIR
 mv ../public/* .
 
